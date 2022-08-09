e020d7a161e48197b3fcb7fc223bf1c14049fc4d
jon-stutler <82524410+jon-stutler@users.noreply.github.com>
2022-08-08 22:42:50 -0500

    builds basic functionality of etch-a-sketch

---
+++ script.js
@@ -0,0 +1,28 @@
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div')
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mousemove', () => {
+            square.style.backgroundColor = 'black';
+        });
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend' , square);
+    }
+}
+
+populateBoard(16);
+
+function changeSize(input) {
+    if(input >= 4 || input <= 100){
+        populateBoard(input);
+    }
+    else {
+        console.log('range is 4-100 squares')
+    }
+}
